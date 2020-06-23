468d9344de21cc9893c14a1329c64f3c1abedb95
ANIRUDH <anirudhatodaria11@gmail.com>
2020-06-23 15:41:50 +0530

    complete working without hero full height

---
+++ app.js
@@ -0,0 +1,74 @@
+const pixaBayKey = "17172717-b019c6a33c2b5e7370880efaa";
+const openWeatherKey = "97fc3552d0eaead441789ea0bd867726";
+
+//responsive navbar
+const navBtn = document.querySelector(".navbar-burger");
+const navMenu = document.querySelector(".navbar-menu");
+navBtn.addEventListener("click", () => {
+  navBtn.classList.toggle("is-active");
+  navMenu.classList.toggle("is-active");
+});
+
+
+
+//fetch city data from openweather
+const data = async (city) => {
+  const dataFetch =  await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${openWeatherKey}`);
+  return await dataFetch.json();
+};
+
+//fetch image
+const imageData = async (city) => {
+  const dataFetch = await fetch(`https://pixabay.com/api/?key=${pixaBayKey}&q=${city}&image_type=photo&pretty=true`)
+    return await dataFetch.json();
+};
+
+//select buttons
+const cityEntered = document.querySelector(".cityName");
+const submitBtn = document.querySelector(".submitBtn");
+const cardHolder = document.querySelector(".card");
+
+//submit event
+submitBtn.addEventListener("click",  async () => {
+  const city = cityEntered.value;
+  const weatherData = await data(city);
+  const imgData = await imageData(city);
+  cardHolder.innerHTML =`
+                    <header class="card-header">
+                        <p class="card-header-title">
+                            ${weatherData.name}
+                        </p>
+                    </header>
+                    <div class="card-content">
+                        <div class="card-image">
+                            <figure class="image is-4by3">
+                                <img src=${imgData.hits[0].largeImageURL} alt="img">
+                            </figure>
+                        </div>
+                        <div class="content">
+                            <table class="table">
+                                <tr>
+                                    <td>Current Temperature</td>
+                                    <td><strong>${weatherData.main.temp} °С</strong></td>
+                                </tr>
+                                <tr>
+                                    <td>Minimum Temperature</td>
+                                    <td><strong>${weatherData.main.temp_min} °С</strong></td>
+                                </tr>
+                                <tr>
+                                    <td>Maximum Temperature</td>
+                                    <td><strong>${weatherData.main.temp_max} °С</strong></td>
+                                </tr>
+                                <tr>
+                                    <td>Humidity</td>
+                                    <td><strong>${weatherData.main.humidity}%</strong></td>
+                                </tr>
+                                <tr>
+                                    <td>Sky</td>
+                                    <td><strong>${weatherData.weather[0].description}</strong></td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+  `;
+});
